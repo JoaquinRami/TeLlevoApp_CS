eb84ba2ed8c8e59cffb8b6a66ee2e729d793a5ee
JoaquinRami <jo.ramirezl@duocuc.cl>
2024-10-10 19:41:36 -0300

    login fix

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -26,12 +26,12 @@ export class LoginPage implements OnInit {
   }
 
   validar() {
-    if (this.nombre === 'conductor' && this.password === this.storedPassword || this.password === "1234") {
+    if (this.nombre === 'conductor' && this.password === this.storedPassword || this.nombre === 'conductor' && this.password === "1234") {
       console.log('Bienvenido, conductor');
       localStorage.setItem('usuario', this.nombre);
 
       this.navCtrl.navigateForward(['/c-home']);
-    } else if (this.nombre === 'pasajero' && this.password === this.storedPassword || this.password === "1234") {
+    } else if (this.nombre === 'pasajero' && this.password === this.storedPassword || this.nombre === 'pasajero' && this.password === "1234") {
       console.log('Bienvenido, Pasajero');
       localStorage.setItem('usuario', this.nombre);
 
